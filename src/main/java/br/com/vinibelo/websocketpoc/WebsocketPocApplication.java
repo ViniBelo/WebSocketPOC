7fa66d4433e0d4c0fb99b413fcb7a0c3424bce1f
ViniBelo <vinialvesbelo@outlook.com>
2025-11-07 21:23:34 -0300

    Initial commit

---
+++ src/main/java/br/com/vinibelo/websocketpoc/WebsocketPocApplication.java
@@ -0,0 +1,13 @@
+package br.com.vinibelo.websocketpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketPocApplication.class, args);
+    }
+
+}
